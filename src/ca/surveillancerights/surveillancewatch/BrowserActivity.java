f08643bf39bb583cc55bfdbaf08e70eb5da6fa74
Matt Zukowski <matt@zukowski.ca>
2012-09-14 10:47:34 -0400

    photo capture is now partially implemented

--- src/ca/surveillancerights/surveillancewatch/BrowserActivity.java
+++ src/ca/surveillancerights/surveillancewatch/BrowserActivity.java
@@ -1,8 +1,13 @@
 package ca.surveillancerights.surveillancewatch;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.webkit.GeolocationPermissions;
@@ -13,6 +18,12 @@ import android.webkit.WebView;
 public class BrowserActivity extends Activity {
 	
 	final static String DEFAULT_VEOS_URL = "http://mobile.watch.surveillancerights.ca/";
+	//final static String DEFAULT_VEOS_URL = "http://192.168.222.114:8000/";
+	
+	private static final int TAKE_PICTURE = 0;
+	
+	private static final String photoFolderPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SurveillanceRights";
+	private static final String photoFilePath = "last_photo.jpg";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,9 @@ public class BrowserActivity extends Activity {
 			}
 		});
 		
+		// add Camera interface for taking photos
+		browser.addJavascriptInterface(this, "Camera");
+		
 		String url = getVeosUrl()+"/app.html#/overview-map.html";
 		Log.v(BrowserActivity.class.getName(), "Loading: "+url);
 		browser.loadUrl(url);
@@ -63,5 +77,35 @@ public class BrowserActivity extends Activity {
 			appUrl = appUrl.replaceFirst("/$", "");
 		
 		return appUrl;
+	}
+	
+	public void takePhoto(final String callback) {
+	    Log.v("Camera Plugin", "Starting takePhoto callback");
+
+	    Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+	    //intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(photoFolderPath, photoFilePath)));
+	    startActivityForResult(intent, TAKE_PICTURE);
+	}
+	
+	public String getPhotoUri() {
+	    return Uri.fromFile(new File(photoFolderPath, photoFilePath)).toString();
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+
+	    switch (requestCode) {
+	        case TAKE_PICTURE:
+	            if (resultCode == Activity.RESULT_OK) {
+	                //Do whatever you need to do when the camera returns
+	                //This is after the picture is already saved, we return to the page
+	            	Log.v("BrowserActivity", "Got photo!");
+	            }
+	            break;
+	        default:
+	            Log.v("Camera", "Something strange happened...");
+	            break;
+	    }
 	}
 }
